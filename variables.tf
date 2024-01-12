cfadd062560e3ab87ab4e4721a33b772267bd162
Kunal <kunalbhat6767@gmail.com>
2024-01-12 21:03:28 +0530

    workflows

--- variables.tf
+++ variables.tf
@@ -1,12 +1,4 @@
 # VPC
-variable "vpc_cidr" {
-  description = "The CIDR block for the VPC."
-  type        = string
-}
-variable "subnet_cidr" {
-  description = "The CIDR block for the subnet."
-  type        = list(string)
-}
 variable "subnet_names" {
   description = "The names for the subnet."
   type        = list(string)
